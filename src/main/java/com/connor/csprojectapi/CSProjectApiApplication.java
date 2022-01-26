6973569ebc822c2d4f52688f348e20896cf581af
Connor <84043753+readUTF@users.noreply.github.com>
2022-01-26 14:33:54 +0000

    initial commit

---
+++ src/main/java/com/connor/csprojectapi/CSProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.connor.csprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSProjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSProjectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/connor/csprojectapi/CsProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connor.csprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
